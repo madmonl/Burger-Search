d2efcd682bbe004e5e3e48aca90388a0e87ee97a
liadm <liadm@wix.com>
2020-10-02 21:11:32 +0300

    Add drawer component

--- src/components/FoodDictionary/FoodDictionary.js
+++ src/components/FoodDictionary/FoodDictionary.js
@@ -1,9 +1,25 @@
 import React from "react";
-import { Drawer, Divider } from "@material-ui/core";
+import {
+  Drawer,
+  Divider,
+  Collapse,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+} from "@material-ui/core";
 import { useStyles } from "./styles";
+import { StarBorder } from "@material-ui/icons";
 
 export default function FoodDictionary() {
+  const [open, setOpen] = React.useState(true);
   const classes = useStyles();
+
+  const handleClick = () => {
+    setOpen(!open);
+  };
+
   return (
     <Drawer
       direction="rtl"
@@ -16,6 +32,29 @@ export default function FoodDictionary() {
     >
       <div className={classes.toolbar} />
       <Divider />
+      <List
+        component="nav"
+        aria-labelledby="nested-list-subheader"
+        subheader={
+          <ListSubheader component="div" id="nested-list-subheader">
+            בחר מרכיבים ומסעדות שאתה אוהב
+          </ListSubheader>
+        }
+      >
+        <ListItem button onClick={handleClick}>
+          <ListItemText primary="רשימת מרכיבים" />
+        </ListItem>
+        <Collapse in={open} timeout="auto" unmountOnExit>
+          <List component="div" disablePadding>
+            <ListItem className={classes.nested}>
+              <ListItemIcon>
+                <StarBorder />
+              </ListItemIcon>
+              <ListItemText primary="Starred" />
+            </ListItem>
+          </List>
+        </Collapse>
+      </List>
     </Drawer>
   );
 }
--- src/components/FoodDictionary/styles.js
+++ src/components/FoodDictionary/styles.js
@@ -3,16 +3,12 @@ import { makeStyles } from "@material-ui/core/styles";
 const drawerWidth = 300;
 
 export const useStyles = makeStyles((theme) => ({
-  root: {
-    display: "flex",
-  },
   appBar: {
     width: `calc(100% - ${drawerWidth}px)`,
     marginLeft: drawerWidth,
   },
   drawer: {
     width: drawerWidth,
-    flexShrink: 0,
   },
   drawerPaper: {
     width: drawerWidth,
@@ -23,5 +19,8 @@ export const useStyles = makeStyles((theme) => ({
     flexGrow: 1,
     backgroundColor: theme.palette.background.default,
     padding: theme.spacing(3),
+  },
+  nested: {
+    paddingLeft: theme.spacing(4),
   },
 }));
